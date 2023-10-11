0dca2c197781a409f4403b6edcbdd8a324eb548a
macastil <gmacastil@gmail.com>
2023-10-10 20:56:53 -0500

    ajuste de controller

--- src/main/java/com/macastil/infrastructure/api/MicroController.java
+++ src/main/java/com/macastil/infrastructure/api/MicroController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.macastil.application.Operaciones;
@@ -12,19 +13,25 @@ import com.macastil.domain.Pesos;
 
 @RestController
 public class MicroController {
-	
+
 	@Value("${trm}")
 	private int trm;
-	
+
 	private static final Logger log = LoggerFactory.getLogger(MicroController.class);
 
 	@GetMapping("/dolares/{dolares}")
 	public Pesos getPesos(@PathVariable int dolares) {
-		Operaciones op =new Operaciones();
+		Operaciones op = new Operaciones();
 		Pesos p = op.getPesos(dolares, trm);
-		//System.out.println("{ 'dolares' :'"+ dolares + "','resultado': " + p.getResultado() + "'}");
+		// System.out.println("{ 'dolares' :'"+ dolares + "','resultado': " +
+		// p.getResultado() + "'}");
 		log.info("dolares: " + dolares);
 		return p;
+	}
+
+	@PutMapping("/")
+	public void setValue() {
+		log.info("setValue");
 	}
 
 }
